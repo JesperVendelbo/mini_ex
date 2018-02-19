fe9c29910fd9dd752c543f4984f45d71c0efd801
JesperVendelbo <36197805+JesperVendelbo@users.noreply.github.com>
2018-02-19 13:04:13 +0100

    Add files via upload
    new

--- mini_ex2/empty-example/sketch.js
+++ mini_ex2/empty-example/sketch.js
@@ -1,56 +1,37 @@
-var slide = 500;
-var increase = 10;
+var slide = 500; //var for ellipsen
+var increase = 10;//var for forstørrelse af tekst
 
 function setup() {
   createCanvas(600,450);
 
   }
-
-
-
-  function draw() {
+ function draw() {
     background(150,100,0);
-
-
-    if(slide < 140) {
-    fill(150,200,40);
-    }
-
     fill(30,200,30);
     rect(64,360,20,20);
     rect(64,400,20,20);
     rect(64,320,20,20);
-
     rect(102,383,20,20);
     rect(102,342,20,20);
-    rect(140,363,20,20); //forreste rect
+    rect(140,363,20,20);
 
-    ellipse(slide,370,50,50);
+    ellipse(slide,370,50,50); //indsætter slide på x-aksens plads, da den er deklæret som 500
 
-      slide = slide - 1,5;
-if(slide < 140) {
+      slide = slide - 1,5; //hvor hurtigt ellipsen skal rykke sig
+if(slide < 140) {  //hvis ellipsen under 140 på x-aksen
 
-  textSize(increase);
+  textSize(increase); // increse på størrelsen af teksten, da den er deklæret som 10
   text('STRIIIKE!!',100,100,100,100);
-  increase = increase + 1;
+  increase = increase + 1; // teksten forstørrer hele tiden
 
   }
-  if(slide < 140){
-
-  }
-
 
       }
 
 
 
-
-
-
-
-
-    function mousePressed() {
-    console.log(mouseX,mouseY);
+function mousePressed() {
+    console.log(mouseX,mouseY); // se hvor på y og x-aksen musen befinder sig
 
 
 
